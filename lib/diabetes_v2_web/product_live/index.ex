249c8296b15ed6804895f18e31f7c8c841eb35e0
Andrew Berry <65485518+Exhibit-A-LUA@users.noreply.github.com>
2025-10-08 12:46:30 +0530

    full pagination working with product search

--- lib/diabetes_v2_web/product_live/index.ex
+++ lib/diabetes_v2_web/product_live/index.ex
@@ -1,7 +1,9 @@
 defmodule DiabetesV2Web.ProductLive.Index do
   use DiabetesV2Web, :live_view
+  ## Required for Ash.Query.filter/2 in Ash 3.0
+  @compile {:no_warn_underscore, Ash.Query}
+  import Ash.Query
 
-  @impl true
   def render(assigns) do
     ~H"""
     <Layouts.app flash={@flash}>
@@ -13,6 +15,28 @@ defmodule DiabetesV2Web.ProductLive.Index do
           </.button>
         </:actions>
       </.header>
+
+      <div class="mb-4 flex items-center">
+        <.form for={@search_form} phx-change="search">
+          <input
+            type="text"
+            name="query"
+            value={@search_query}
+            placeholder="Search products by name..."
+            phx-debounce="300"
+            class="border rounded px-3 py-1 mr-2 w-64"
+          />
+        </.form>
+        <.button
+          variant="primary"
+          phx-click="clear_search"
+          disabled={@search_query == ""}
+        >
+          Clear
+        </.button>
+
+        <div :if={@loading} class="ml-2">...</div>
+      </div>
 
       <.table
         id="products"
@@ -35,19 +59,19 @@ defmodule DiabetesV2Web.ProductLive.Index do
           <div class="sr-only">
             <.link navigate={~p"/products/#{product}"}>Show</.link>
           </div>
-
           <.link navigate={~p"/products/#{product}/edit"}>Edit</.link>
         </:action>
 
-        <:action :let={{id, product}}>
+        <:action :let={{_id, product}}>
           <.link
-            phx-click={JS.push("delete", value: %{id: product.id}) |> hide("##{id}")}
+            phx-click={JS.push("delete", value: %{id: product.id}) |> hide("##{product.id}")}
             data-confirm="Are you sure?"
           >
             Delete
           </.link>
         </:action>
       </.table>
+
       <div class="mt-4 flex justify-between">
         <.button
           variant="primary"
@@ -68,17 +92,12 @@ defmodule DiabetesV2Web.ProductLive.Index do
     """
   end
 
-  @impl true
   def mount(_params, _session, socket) do
-    # Initialize pagination state
     offset = 0
     limit = 50
+    query = ""
 
-    # Fetch the initial page
-    page_result =
-      DiabetesV2.Products.Product
-      |> Ash.Query.for_read(:with_types)
-      |> Ash.read!(page: [limit: limit, offset: offset], actor: socket.assigns[:current_user])
+    page = fetch_page(socket.assigns[:current_user], query, offset, limit)
 
     {:ok,
      socket
@@ -86,57 +105,102 @@ defmodule DiabetesV2Web.ProductLive.Index do
      |> assign_new(:current_user, fn -> nil end)
      |> assign(:offset, offset)
      |> assign(:limit, limit)
-     |> assign(:has_more, page_result.more?)
-     |> stream(:products, page_result.results, reset: true)}
+     |> assign(:search_query, query)
+     |> assign(:search_form, to_form(%{}, as: :search))
+     |> assign(:has_more, page.more?)
+     |> assign(:loading, false)
+     |> stream(:products, page.results, reset: true)}
   end
 
-  @impl true
+  def handle_event("search", %{"query" => query}, socket) do
+    socket = assign(socket, :loading, true)
+    new_offset = 0
+    page = fetch_page(socket.assigns.current_user, query, new_offset, socket.assigns.limit)
+    socket = socket |> stream(:products, page.results, reset: true)
+
+    {:noreply,
+     socket
+     |> assign(:loading, false)
+     |> assign(:search_query, query)
+     |> assign(:offset, new_offset)
+     |> assign(:has_more, page.more?)}
+  end
+
+  def handle_event("clear_search", _params, socket) do
+    handle_event("search", %{"query" => ""}, socket)
+  end
+
   def handle_event("next_page", _params, socket) do
-    # Calculate new offset
     new_offset = socket.assigns.offset + socket.assigns.limit
 
-    # Fetch the next page
-    page_result =
-      DiabetesV2.Products.Product
-      |> Ash.Query.for_read(:with_types)
-      |> Ash.read!(
-        page: [limit: socket.assigns.limit, offset: new_offset],
-        actor: socket.assigns.current_user
+    page =
+      fetch_page(
+        socket.assigns.current_user,
+        socket.assigns.search_query,
+        new_offset,
+        socket.assigns.limit
       )
 
     {:noreply,
      socket
      |> assign(:offset, new_offset)
-     |> assign(:has_more, page_result.more?)
-     |> stream(:products, page_result.results, reset: true)}
+     |> assign(:has_more, page.more?)
+     |> stream(:products, page.results, reset: true)}
   end
 
-  @impl true
   def handle_event("previous_page", _params, socket) do
-    # Calculate new offset (ensure it doesn't go negative)
     new_offset = max(socket.assigns.offset - socket.assigns.limit, 0)
 
-    # Fetch the previous page
-    page_result =
-      DiabetesV2.Products.Product
-      |> Ash.Query.for_read(:with_types)
-      |> Ash.read!(
-        page: [limit: socket.assigns.limit, offset: new_offset],
-        actor: socket.assigns.current_user
+    page =
+      fetch_page(
+        socket.assigns.current_user,
+        socket.assigns.search_query,
+        new_offset,
+        socket.assigns.limit
       )
 
     {:noreply,
      socket
      |> assign(:offset, new_offset)
-     |> assign(:has_more, page_result.more?)
-     |> stream(:products, page_result.results, reset: true)}
+     |> assign(:has_more, page.more?)
+     |> stream(:products, page.results, reset: true)}
   end
 
-  @impl true
   def handle_event("delete", %{"id" => id}, socket) do
     product = Ash.get!(DiabetesV2.Products.Product, id, actor: socket.assigns.current_user)
     Ash.destroy!(product, actor: socket.assigns.current_user)
 
-    {:noreply, stream_delete(socket, :products, product)}
+    page =
+      fetch_page(
+        socket.assigns.current_user,
+        socket.assigns.search_query,
+        socket.assigns.offset,
+        socket.assigns.limit
+      )
+
+    {:noreply,
+     socket
+     |> stream_delete(:products, product)
+     |> assign(:has_more, page.more?)
+     |> stream(:products, page.results, reset: true)}
+  end
+
+  defp fetch_page(actor, query, offset, limit) do
+    base =
+      DiabetesV2.Products.Product
+      |> Ash.Query.for_read(:with_types)
+
+    query =
+      if query == "" || query == nil do
+        base
+      else
+        escaped = String.replace(query, "%", "\\%")
+        filter(base, name: [ilike: "%#{escaped}%"])
+      end
+
+    Ash.read!(query,
+      page: [limit: limit, offset: offset],
+      actor: actor
+    )
   end
 end
